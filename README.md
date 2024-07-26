e7e50f8417d7e1a2368693e447950088b3ecd57a
GGN_2015 <premierbob@qq.com>
2024-07-26 11:56:26 +0800

    fix:wrong_encode_method

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # get_in_out_code
 分析 PD_CODE 中，每一条弧线的出入走向。
+
--- src/get_in_out_code.py
+++ src/get_in_out_code.py
@@ -8,28 +8,24 @@ def __status_reverse(status: str):
     else:
         return "IN"
 
+def __get_next(total_n:int, val_now:int) -> int:
+    if val_now == total_n:
+        return 1
+    else:
+        return val_now + 1
+
 def get_in_out_code(pd_code:list) -> list:
+    total_n = len(pd_code) * 2
     in_out_code = []
-    for _ in pd_code: # 第一个弧线一定是 IN,第三个弧线一定是 OUT
+    for idx in range(len(pd_code)): # 第一个弧线一定是 IN,第三个弧线一定是 OUT
         in_out_code.append(["IN", None, "OUT", None])
-    in_out_status = {}
-    for i in range(len(pd_code)):
-        for j in range(4): # 考虑每一个弧线，是否做过入弧线，是否做过出弧线
-            if in_out_code[i][j] is not None:
-                in_out_status[pd_code[i][j]] = in_out_code[i][j]
-    need_next_turn = True
-    while need_next_turn:
-        need_next_turn = False # 正常来说，一轮计算就能结束
-        for i in range(len(pd_code)):
-            for j in range(4):
-                if in_out_code[i][j] is None:
-                    if in_out_status.get(pd_code[i][j]) is not None:
-                        in_out_code[i][j] = __status_reverse(in_out_status[pd_code[i][j]])
-                    elif in_out_status.get(pd_code[i][(j+2) % 4]) is not None:
-                        in_out_code[i][j] = __status_reverse(in_out_status[pd_code[i][(j + 2) % 4]])
-                    else:
-                        need_next_turn = True # 说明需要进行下一轮计算
+        if pd_code[idx][1] == __get_next(total_n, pd_code[idx][3]):
+            in_out_code[-1][1] = "OUT"
+            in_out_code[-1][3] = "IN"
+        else:
+            in_out_code[-1][1] = "IN"
+            in_out_code[-1][3] = "OUT"
     return in_out_code
 
 if __name__ == "__main__":
-    print(get_in_out_code([[1, 4, 2, 5], [3, 6, 4, 1], [5, 2, 6, 3]]))
+    print(get_in_out_code([[4,1,5,2],[15,1,16,22],[10,4,11,3],[2,12,3,11],[9,16,10,17],[7,18,8,19],[17,8,18,9],[19,12,20,13],[5,15,6,14],[13,20,14,21],[21,6,22,7]]))
